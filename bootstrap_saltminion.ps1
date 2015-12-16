c9ecb4f82e3a5eab6b26cdf3c0afd137f2c868a4
Seth Miller <seth@sethmiller.me>
2015-12-16 15:43:56 -0600

    fix computername variable typo

--- bootstrap_saltminion.ps1
+++ bootstrap_saltminion.ps1
@@ -14,5 +14,5 @@ Write-Verbose 'Installing Salt'
 Start-Process $InstallerFile -Wait `
                              -NoNewWindow `
                              -PassThru `
-                             -ArgumentList "/S /master=$MasterHost /minion-name=$env:COMPUTERNAME1"
+                             -ArgumentList "/S /master=$MasterHost /minion-name=$env:COMPUTERNAME"
 Write-Verbose "Salt is installed"
