d6397a287e8dd5818504cd76bff1a3853fdcea42
Seth Miller <seth@sethmiller.me>
2016-03-21 21:56:26 -0500

    remove repo removal

--- linux_salt_bootstrap.sh
+++ linux_salt_bootstrap.sh
@@ -6,17 +6,6 @@ set -u
 
 readonly MASTER_CONFIG='file_roots:,  base:,    - /srv/salt,    - /srv/formulas,    - /srv/salt/roles,pillar_roots:,  base:,    - /srv/pillar,  dev:,    - /srv/pillar/dev,  production:,    - /srv/pillar/production'
 
-function install_centos_repo() {
-  rm -f /etc/yum.repos.d/CentOS-* /etc/yum.repos.d/OpenLogic.repo
-  echo '[base]' > /etc/yum.repos.d/Centos-Base.repo
-  echo 'name=CentOS-$releasever - Base' >> /etc/yum.repos.d/Centos-Base.repo
-  echo 'mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os' >> /etc/yum.repos.d/Centos-Base.repo
-  echo 'gpgcheck=1' >> /etc/yum.repos.d/Centos-Base.repo
-  echo 'gpgkey=http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-7' >> /etc/yum.repos.d/Centos-Base.repo
-  echo 'protect=1' >> /etc/yum.repos.d/Centos-Base.repo
-  yum clean all
-}
-
 
 function install_salt_repo() {
   rpm --import https://repo.saltstack.com/yum/redhat/7/x86_64/latest/SALTSTACK-GPG-KEY.pub
@@ -53,7 +42,6 @@ function install_salt_minion() {
 
 
 main() {
-  install_centos_repo
   if [[ $1 == 'master' ]]; then
      install_salt_master
   else
