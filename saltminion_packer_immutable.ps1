a7b29a67240293442354f4c7fb0d546a770b28d2
Greg Grandbois <ifutant@me.com>
2018-06-22 19:50:57 -0400

    Adding Spaces

--- saltminion_packer_immutable.ps1
+++ saltminion_packer_immutable.ps1
@@ -4,8 +4,8 @@ $ConfigFile = 'C:\salt\conf\minion'
 $IdFile = 'C:\salt\conf\minion_id'
 $minionId = $env:COMPUTERNAME.ToLower()
 
-Set-Content $ConfigFile "master:$MasterHost"
-Add-Content $ConfigFile "startup_states:highstate"
+Set-Content $ConfigFile "master: $MasterHost"
+Add-Content $ConfigFile "startup_states: highstate"
 Set-Content $IdFile "$minionId"
 Set-Service "salt-minion" -StartupType "Automatic"
 Start-Service "salt-minion"
