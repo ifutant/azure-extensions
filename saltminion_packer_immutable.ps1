1117947aae34ece61d9d26e7cb880f3a8f9fb2e9
Greg Grandbois <ifutant@me.com>
2018-06-22 13:38:13 -0400

    Add files via upload
    Immutable Packer Config

---
+++ saltminion_packer_immutable.ps1
@@ -0,0 +1,11 @@
+param([string]$MasterHost)
+Write-Verbose 'Configuring Salt... please wait'
+$ConfigFile = 'C:\salt\conf\minion'
+$IdFile = 'C:\salt\conf\minion_id'
+$minionId = $env:COMPUTERNAME.ToLower()
+
+Set-Content $ConfigFile "master:$MasterHost"
+Add-Content $ConfigFile "startup_states:highstate"
+Set-Content $IdFile "$minionId"
+Set-Service "salt-minion" -StartupType "Automatic"
+Start-Service "salt-minion"
