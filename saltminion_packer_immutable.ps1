9e76940df495bef0edbadd8a76b4d1f1cf8661a4
Greg Grandbois <ifutant@me.com>
2018-07-16 11:26:04 -0400

    Update saltminion_packer_immutable.ps1

--- saltminion_packer_immutable.ps1
+++ saltminion_packer_immutable.ps1
@@ -14,7 +14,7 @@ $nms_app_id = $nms_app_zip
 $grainString = @"
 grains:
   nms_app_id: $nms_app_id
-  deployment_method: packer
+  deployment_method: packer_post_deploy
 "@
 
 Set-Content $ConfigFile "master: $MasterHost"
