de7daebe3e8ca8aaf10462b9ad82ab59c8968e96
Greg Grandbois <ifutant@me.com>
2018-07-15 22:47:59 -0400

    Update saltminion_packer_immutable.ps1

--- saltminion_packer_immutable.ps1
+++ saltminion_packer_immutable.ps1
@@ -1,11 +1,19 @@
-param([string]$MasterHost)
+param([string]$MasterHost),
+param([string]$nms_app_zip)
 Write-Verbose 'Configuring Salt... please wait'
 $ConfigFile = 'C:\salt\conf\minion'
 $IdFile = 'C:\salt\conf\minion_id'
 $minionId = $env:COMPUTERNAME.ToLower()
+$nms_app_id = $nms_app_zip
+
+$grainString = @"
+grains:
+  nms_app_id: $nms_app_id
+"@
 
 Set-Content $ConfigFile "master: $MasterHost"
 Add-Content $ConfigFile "startup_states: highstate"
+Add-Content $ConfigFile $grainString
 Set-Content $IdFile "$minionId"
 Set-Service "salt-minion" -StartupType "Automatic"
 Start-Service "salt-minion"
