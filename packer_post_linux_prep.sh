ce8ee4ae9c2a24b2ce92089ffbb71e58e308135b
Greg Grandbois <ifutant@me.com>
2018-08-27 17:28:14 -0400

    Update packer_post_linux_prep.sh

--- packer_post_linux_prep.sh
+++ packer_post_linux_prep.sh
@@ -2,11 +2,10 @@
 PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
 
 MASTER=$1
-NMS_APP_ID=$2
-POD=$3
+POD=$2
 REGEX_HOSTNAME="^[a-z]{15}"
 hostname | grep -E "${REGEX_HOSTNAME}" > /etc/salt/minion_id
-printf "%s\n" "master: $MASTER" "startup_states: highstate" "grains:" "  nms_app_id: $NMS_APP_ID" "  pod: $POD" "  deployment_method: packer_post_deploy" > /etc/salt/minion 
+printf "%s\n" "master: $MASTER" "startup_states: highstate" "grains:" "  pod: $POD" "  deployment_method: packer_post_deploy" > /etc/salt/minion 
 systemctl enable salt-minion.service
 systemctl start salt-minion.service
 salt-call saltutil.sync_grains
