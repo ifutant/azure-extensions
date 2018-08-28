0da6517685d49d1743a875eafa73bb5f029f2104
Greg Grandbois <ifutant@me.com>
2018-08-28 14:48:16 -0400

    Update packer_post_linux_prep.sh

--- packer_post_linux_prep.sh
+++ packer_post_linux_prep.sh
@@ -4,6 +4,7 @@ PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
 REGEX_HOSTNAME="^[a-z]{15}"
 
 hostname | grep -E "${REGEX_HOSTNAME}" > /etc/salt/minion_id
+salt-call file.replace '/etc/salt/minion' pattern='packer_pre_deploy' repl='packer_post_deploy'
 salt-call state.highstate -l debug --local
 salt-call state.highstate -l debug --local
 salt-call system.reboot 1
