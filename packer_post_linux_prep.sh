efaab4b30761c788fdf154004f1c7609f7e2d12b
Greg Grandbois <ifutant@me.com>
2018-08-28 12:50:11 -0400

    Update packer_post_linux_prep.sh

--- packer_post_linux_prep.sh
+++ packer_post_linux_prep.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
 
-MASTER=$1
-POD=$2
 REGEX_HOSTNAME="^[a-z]{15}"
+
 hostname | grep -E "${REGEX_HOSTNAME}" > /etc/salt/minion_id
-printf "%s\n" "master: $MASTER" "startup_states: highstate" "grains:" "  pod: $POD" "  deployment_method: packer_post_deploy" > /etc/salt/minion 
+printf "%s\n" "startup_states: highstate" "grains:" "  deployment_method: packer_post_deploy" > /etc/salt/minion 
 systemctl enable salt-minion.service
 systemctl start salt-minion.service
 salt-call saltutil.sync_grains
 salt-call saltutil.refresh_pillar
-salt-call state.highstate
-salt-call state.highstate
+salt-call state.highstate -l debug --local
+salt-call state.highstate -l debug --local
+salt-call system.reboot 1
 exit 0
