8346e8b0160c01983927ba4068fc97aafa03ba77
Greg Grandbois <ifutant@me.com>
2018-08-28 14:18:26 -0400

    Update packer_post_linux_prep.sh

--- packer_post_linux_prep.sh
+++ packer_post_linux_prep.sh
@@ -6,8 +6,6 @@ REGEX_HOSTNAME="^[a-z]{15}"
 hostname | grep -E "${REGEX_HOSTNAME}" > /etc/salt/minion_id
 systemctl enable salt-minion.service
 systemctl start salt-minion.service
-salt-call saltutil.sync_grains
-salt-call saltutil.refresh_pillar
 salt-call state.highstate -l debug --local
 salt-call state.highstate -l debug --local
 salt-call system.reboot 1
