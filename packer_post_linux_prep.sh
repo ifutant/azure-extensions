3c0c43eb7b7f41894c6b34dd03e5c2b03ec41112
Greg Grandbois <ifutant@me.com>
2018-10-10 11:57:22 -0400

    Update packer_post_linux_prep.sh

--- packer_post_linux_prep.sh
+++ packer_post_linux_prep.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
-
+SALTDIRECTORY=/deployTemp
 REGEX_HOSTNAME="^[a-z]{15}"
 
 hostname | grep -E "${REGEX_HOSTNAME}" > /etc/salt/minion_id
+if [ -d "$SALTDIRECTORY" ]; then
 salt-call file.replace '/etc/salt/minion' pattern='packer_pre_deploy' repl='packer_post_deploy'
 salt-call state.highstate -l debug --local
 salt-call state.highstate -l debug --local
 salt-call system.reboot 1
-rm -rf /deployTemp
+rm -rf $SALTDIRECTORY
 systemctl disable salt-minion
+fi
 exit 0
