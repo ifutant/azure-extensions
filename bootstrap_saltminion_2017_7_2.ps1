3a0b08131aeccbace98253a07b70f076f624d097
Greg Grandbois <ifutant@me.com>
2019-08-15 15:05:09 -0400

    Create bootstrap_saltminion_2017_7_2.ps1

---
+++ bootstrap_saltminion_2017_7_2.ps1
@@ -0,0 +1,24 @@
+param([string]$MasterHost)
+
+$SourceURI = 'https://repo.saltstack.com/windows/Salt-Minion-2017.7.2-Py2-AMD64-Setup.exe'
+
+Write-Verbose 'Installing Salt... please wait'
+$InstallerFile = 'C:\Packages\Salt.exe'
+$ConfigFile = 'C:\salt\conf\minion'
+
+Write-Verbose "Downloading salt installer from $SourceURI to $InstallerFile"
+$WebClient = New-Object System.Net.WebClient
+$webclient.DownloadFile($SourceURI, $InstallerFile)
+Write-Verbose 'Salt installer downloaded.'
+
+Write-Verbose 'Installing Salt'
+
+$minionId = $env:COMPUTERNAME.ToLower()
+
+Start-Process $InstallerFile -Wait `
+                             -NoNewWindow `
+                             -PassThru `
+                             -ArgumentList "/S /master=$MasterHost /minion-name=$minionId"
+Write-Verbose "Salt is installed"
+Add-Content $ConfigFile "startup_states: highstate"
+Restart-Service salt-minion
